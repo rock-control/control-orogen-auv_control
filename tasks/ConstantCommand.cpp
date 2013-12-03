dc8c8516cd3e1e5df289c72ac18292b1e994610d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-03 11:52:03 +0100

    add a task to generate constant commands

---
+++ tasks/ConstantCommand.cpp
@@ -0,0 +1,55 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "ConstantCommand.hpp"
+
+using namespace auv_control;
+
+ConstantCommand::ConstantCommand(std::string const& name, TaskCore::TaskState initial_state)
+    : ConstantCommandBase(name, initial_state)
+{
+}
+
+ConstantCommand::ConstantCommand(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
+    : ConstantCommandBase(name, engine, initial_state)
+{
+}
+
+ConstantCommand::~ConstantCommand()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See ConstantCommand.hpp for more detailed
+// documentation about them.
+
+bool ConstantCommand::configureHook()
+{
+    if (! ConstantCommandBase::configureHook())
+        return false;
+    return true;
+}
+bool ConstantCommand::startHook()
+{
+    if (! ConstantCommandBase::startHook())
+        return false;
+    return true;
+}
+void ConstantCommand::updateHook()
+{
+    _cmd_out.write(_cmd.get());
+    ConstantCommandBase::updateHook();
+}
+void ConstantCommand::errorHook()
+{
+    ConstantCommandBase::errorHook();
+}
+void ConstantCommand::stopHook()
+{
+    ConstantCommandBase::stopHook();
+}
+void ConstantCommand::cleanupHook()
+{
+    ConstantCommandBase::cleanupHook();
+}
